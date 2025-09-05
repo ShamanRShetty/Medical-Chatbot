970368e76283b2d8007a7fade66d066f83838cf9
Shaman R Shetty <shamanshetty2211@gmail.com>
2025-09-05 14:14:56 +0530

    Requirements Added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup (
+    name="Medical-Chatbot",
+    version="0.1.0",
+    author="Shaman R Shetty",
+    author_email="shamanshetty2211@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
